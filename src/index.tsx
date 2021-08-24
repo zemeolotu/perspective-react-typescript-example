cf42cabe69277bd38b38b2896553a9474b166035
Zeme <zeme.olotu@jpmorgan.com>
2021-08-24 09:50:21 -0400

    Add workspace example

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,102 @@
-/******************************************************************************
- *
- * Copyright (c) 2017, the Perspective Authors.
- *
- * This file is part of the Perspective library, distributed under the terms of
- * the Apache License 2.0.  The full license can be found in the LICENSE file.
- *
- */
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import {useEffect, useRef} from "react";
-import perspective, {Table} from "@finos/perspective";
+import { useEffect, useRef } from "react";
+import perspective, { Table } from "@finos/perspective";
 
-import "@finos/perspective-viewer";
+
+import "@finos/perspective-workspace";
 import "@finos/perspective-viewer-datagrid";
 import "@finos/perspective-viewer-d3fc";
 import "./index.css";
-import "@finos/perspective-viewer/dist/umd/material.css";
-import {HTMLPerspectiveViewerElement, PerspectiveViewerOptions} from "@finos/perspective-viewer";
+import "@finos/perspective-workspace/dist/umd/material.dark.css";
+
+declare global {
+    namespace JSX {
+        interface IntrinsicElements {
+            'perspective-workspace': any;
+        }
+    }
+
+    interface Document {
+        createElement(tagName: "perspective-workspace"): any;
+    }
+}
+
+
+
+const DEFAULT_LAYOUT = {
+    detail: {
+        main: {
+            type: "split-area",
+            orientation: "horizontal",
+            children: [
+                {
+                    type: "tab-area",
+                    widgets: ["One"],
+                    currentIndex: 0
+                },
+                {
+                    type: "split-area",
+                    orientation: "vertical",
+                    children: [
+                        {
+                            type: "tab-area",
+                            widgets: ["Two"],
+                            currentIndex: 0
+                        },
+                        {
+                            type: "tab-area",
+                            widgets: ["Three"],
+                            currentIndex: 0
+                        }
+                    ],
+                    sizes: [0.5, 0.5]
+                }
+            ],
+            sizes: [0.5, 0.5]
+        }
+    },
+    mode: "globalFilters",
+    viewers: {
+        One: {
+            plugin: "Heatmap",
+            columns: ["Profit"],
+            "row-pivots": ["Sub-Category"],
+            "column-pivots": ["State"],
+            table: "superstore",
+        },
+        Two: {
+            plugin: "Y Bar",
+            "row-pivots": ["Category"],
+            columns: ["Profit"],
+            "column-pivots": ["Sub-Category"],
+            name: "Profit By Category",
+            table: "superstore",
+        },
+        Three: {
+            "row-pivots": [
+                "State"
+            ],
+            columns: [
+                "Sales",
+                "Profit",
+                null
+            ],
+            plugin: "Treemap",
+            sort: [
+                [
+                    "Profit",
+                    "desc"
+                ]
+            ],
+
+            name: "Sales/Profit By State",
+            table: "superstore",
+
+        }
+    }
+}
+
 
 const worker = perspective.shared_worker();
 
@@ -27,24 +107,20 @@ const getTable = async (): Promise<Table> => {
     return await worker.table(buffer as any);
 };
 
-const config: PerspectiveViewerOptions = {
-    "row-pivots": ["State"]
-};
-
 const App = (): React.ReactElement => {
-    const viewer = useRef<HTMLPerspectiveViewerElement>(null);
+    const workspace = useRef<any>(null);
 
     useEffect(() => {
         getTable().then(table => {
-            if (viewer.current) {
-                viewer.current.load(table);
-                viewer.current.restore(config);
+            if (workspace.current) {
+
+                workspace.current.tables.set("superstore", table);
+                workspace.current.restore(DEFAULT_LAYOUT);
             }
         });
     }, []);
 
-    // You can also the use the stringified config values as attributes
-    return <perspective-viewer ref={viewer} /*row-pivots='["State"]'*/></perspective-viewer>;
+    return <perspective-workspace ref={workspace} ></perspective-workspace>;
 };
 window.addEventListener("load", () => {
     ReactDOM.render(<App />, document.getElementById("root"));
